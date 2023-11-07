900df52fe2d8a344ea9be6e229ce6825b2dc01ca
Abgier Avraha <abgier.avraha@gmail.com>
2023-11-07 16:31:52 +1100

    diacritic stripping

--- src/utils/dictionary.ts
+++ src/utils/dictionary.ts
@@ -17,9 +17,16 @@ const entrySchema = z.object({
 
 export type EntrySchemaType = z.infer<typeof entrySchema>;
 
+// Search queries will be made lower case and have their diacritics removed
+interface DictionaryLookupEntry extends EntrySchemaType {
+  EnglishLowerCase: string;
+  SyriacWithoutDiacritics: string;
+  ArabicWithoutDiacritics: string;
+}
+
 export class Dictionary implements IDictionary {
   private englishMap: Map<string, EntrySchemaType> = new Map();
-  private dictionary: EntrySchemaType[] = [];
+  private dictionary: DictionaryLookupEntry[] = [];
   private isLoaded = false;
 
   load(csv: string) {
@@ -27,7 +34,12 @@ export class Dictionary implements IDictionary {
     const parser = new CsvParser();
 
     // Load dictionary
-    this.dictionary = parser.parse(csv, entrySchema);
+    this.dictionary = parser.parse(csv, entrySchema).map((e) => ({
+      ...e,
+      SyriacWithoutDiacritics: this.removeSyriacDiacritics(e.Syriac),
+      ArabicWithoutDiacritics: this.removeArabicDiacritics(e.Arabic),
+      EnglishLowerCase: e.English.toLowerCase(),
+    }));
 
     // Setup map for exact lookup
     this.dictionary.forEach((e) => {
@@ -47,21 +59,23 @@ export class Dictionary implements IDictionary {
       return [];
     }
 
-    const lowerCaseQuery = query.toLowerCase();
+    const simplifiedQuery = this.removeArabicDiacritics(
+      this.removeSyriacDiacritics(query.toLowerCase())
+    );
 
     const startsWith = this.dictionary.filter((w) => {
       return (
-        w.Arabic.toLowerCase().startsWith(lowerCaseQuery) ||
-        w.English.toLowerCase().startsWith(lowerCaseQuery) ||
-        w.Syriac.toLowerCase().startsWith(lowerCaseQuery)
+        w.ArabicWithoutDiacritics.startsWith(simplifiedQuery) ||
+        w.EnglishLowerCase.startsWith(simplifiedQuery) ||
+        w.SyriacWithoutDiacritics.startsWith(simplifiedQuery)
       );
     });
 
     const includes = this.dictionary.filter((w) => {
       return (
-        w.Arabic.toLowerCase().includes(lowerCaseQuery) ||
-        w.English.toLowerCase().includes(lowerCaseQuery) ||
-        w.Syriac.toLowerCase().includes(lowerCaseQuery)
+        w.ArabicWithoutDiacritics.includes(simplifiedQuery) ||
+        w.EnglishLowerCase.includes(simplifiedQuery) ||
+        w.SyriacWithoutDiacritics.includes(simplifiedQuery)
       );
     });
 
@@ -74,6 +88,41 @@ export class Dictionary implements IDictionary {
     }
 
     return this.englishMap.get(word.english);
+  }
+
+  private removeSyriacDiacritics(word: string): string {
+    return word
+      .split("")
+      .map((letter) => {
+        const code = letter.charCodeAt(0);
+        if (code >= 1792 && code <= 1801) {
+          return "";
+        }
+
+        if (code >= 1840 && code <= 1866) {
+          return "";
+        }
+
+        return letter;
+      })
+      .join("");
+  }
+
+  private removeArabicDiacritics(word: string): string {
+    return word
+      .split("")
+      .map((letter) => {
+        const code = letter.charCodeAt(0);
+        if (code >= 1611 && code <= 1631) {
+          return "";
+        }
+
+        if (code >= 1560 && code <= 1562) {
+          return "";
+        }
+        return letter;
+      })
+      .join("");
   }
 }
 
