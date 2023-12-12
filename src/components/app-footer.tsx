a03bee8a67023bc1a7936b37fbc7036c879c8faf
Abgier Avraha <abgier.avraha@gmail.com>
2023-12-12 20:56:28 +1100

    responsive footer

--- src/components/app-footer.tsx
+++ src/components/app-footer.tsx
@@ -9,31 +9,33 @@ export function AppFooter() {
   return (
     <footer className="py-20 text-center">
       <Container>
-        <div className="flex flex-row gap-2 justify-center items-center text-gray-600 dark:text-gray-300">
-          <div>Provide feedback to</div>
-          <div className="flex flex-row gap-2 justify-center items-center">
-            <Image className="inline dark:hidden" alt="banipal-logo" src={mailLight.src} height={20} width={20} />
-            <Image className="hidden dark:inline" alt="banipal-logo" src={mailDark.src} height={20} width={20} />
-            <a className="hover:underline" href="mailto:ramin12_79@yahoo.com">ramin12_79@yahoo.com</a>
+        <div className="flex flex-col gap-2">
+          <div className="flex sm:flex-row sm:gap-2 gap-0 flex-col justify-center items-center text-gray-600 dark:text-gray-300">
+            <div>Provide feedback to</div>
+            <div className="flex flex-row gap-2 justify-center items-center">
+              <Image className="inline dark:hidden" alt="banipal-logo" src={mailLight.src} height={20} width={20} />
+              <Image className="hidden dark:inline" alt="banipal-logo" src={mailDark.src} height={20} width={20} />
+              <a className="hover:underline" href="mailto:ramin12_79@yahoo.com">ramin12_79@yahoo.com</a>
+            </div>
           </div>
-        </div>
 
-        <div className="flex flex-row gap-2 justify-center items-center text-gray-600 dark:text-gray-300">
-          <div>Website © 2023 Abgier Avraha</div>
-          <div className="flex flex-row gap-2 justify-center items-center">
-            <Image className="inline dark:hidden" alt="banipal-logo" src={githubLight.src} height={20} width={20} />
-            <Image className="hidden dark:inline" alt="banipal-logo" src={githubDark.src} height={20} width={20} />
-            <a className="hover:underline" href="https://github.com/abgier-avraha/Banipal-Assyrian-Dictionary-Website/">
-              GitHub Repo
+          <div className="flex sm:flex-row sm:gap-2 gap-0 flex-col justify-center items-center text-gray-600 dark:text-gray-300">
+            <div>Website © 2023 Abgier Avraha</div>
+            <div className="flex flex-row gap-2 justify-center items-center">
+              <Image className="inline dark:hidden" alt="banipal-logo" src={githubLight.src} height={20} width={20} />
+              <Image className="hidden dark:inline" alt="banipal-logo" src={githubDark.src} height={20} width={20} />
+              <a className="hover:underline" href="https://github.com/abgier-avraha/Banipal-Assyrian-Dictionary-Website/">
+                GitHub Repo
+              </a>
+            </div>
+          </div>
+
+          <div className="text-gray-600 dark:text-gray-300">
+            Definitions are acquired from{" "}
+            <a className="hover:underline" href="https://dictionaryapi.dev/">
+              dictionaryapi.dev
             </a>
           </div>
-        </div>
-
-        <div className="text-gray-600 dark:text-gray-300">
-          Definitions are acquired from{" "}
-          <a className="hover:underline" href="https://dictionaryapi.dev/">
-            dictionaryapi.dev
-          </a>
         </div>
       </Container>
     </footer>
